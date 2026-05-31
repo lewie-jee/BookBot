518083584dd97e823ef9f4aa369bdc9c1455367b
lewie-jee <lewis.shriver@proton.me>
2026-05-31 17:33:59 +0200

    feat: implement structured character data sorting and reporting pipeline
    - Introduce CharacterCount TypedDict for strict dictionary type-hinting
    - Create dict_list helper to map and sort character frequencies in descending order
    - Implement print_report to filter non-alphabetical tokens and output formatted analytics

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_chars
+from stats import count_words, count_chars, dict_list
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -9,9 +9,24 @@ def get_book_text(path_to_file):
 def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_book_text(path_to_file)
+    print_report(file_contents)
+
+def print_report(file_contents):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     count_words(file_contents)
+    print("--------- Character Count -------")
     char_dict = count_chars(file_contents)
-    print(char_dict)
+    char_dict_list = dict_list(char_dict)
+    for char_dict_list_element in char_dict_list:
+        if not char_dict_list_element["char"].isalpha():
+            continue
+        print (f"{char_dict_list_element["char"]}: {char_dict_list_element["num"]}")
+    print("============= END ===============")
+    
+
+
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+from typing import TypedDict
+
 def count_words(file_contents):
     words = file_contents.split()
     num_words = len(words)
@@ -13,3 +15,21 @@ def count_chars(file_contents):
         else:
             char_dict[char] += 1
     return char_dict
+
+class CharacterCount(TypedDict):
+    char: str
+    num: int
+
+def sort_on(char_dict_list_element):
+    return char_dict_list_element["num"]
+
+def dict_list(char_dict: dict) -> list[CharacterCount]:
+    char_dict_list = list()
+    for char_element in char_dict:
+        char_dict_list_element = dict()
+        char_dict_list_element["char"] = char_element
+        char_dict_list_element["num"] = char_dict[char_element]
+        char_dict_list.append(char_dict_list_element)
+    char_dict_list.sort(reverse = True, key = sort_on)
+    return char_dict_list
+    
