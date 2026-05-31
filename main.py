ae4e284d08615e934c92ebb93d8d01a15c004eb0
lewie-jee <lewis.shriver@proton.me>
2026-05-31 12:49:38 +0200

    feat: add file reading logic to print book contents

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+
 def main():
-    print("Hello from bookbot!")
+    path_to_file = "books/frankenstein.txt"
+    file_contents = get_book_text(path_to_file)
+    print(file_contents)
+
 
 
 if __name__ == "__main__":
