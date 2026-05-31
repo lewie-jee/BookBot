695753ec2b1588c2094c253366c66a2af6750b0a
lewie-jee <lewis.shriver@proton.me>
2026-05-31 13:03:38 +0200

    feat: implement word count function and update output summary

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
+def count_words(file_contents):
+    words = file_contents.split()
+    num_words = len(words)
+    msg = f"Found {num_words} total words"
+    print(msg)
+
 
 def main():
     path_to_file = "books/frankenstein.txt"
     file_contents = get_book_text(path_to_file)
-    print(file_contents)
+    count_words(file_contents)
 
 
 
